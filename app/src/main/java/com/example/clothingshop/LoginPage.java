56987a655235334dc7f4b3b0e969c2d7aabcf7e3
amanpreetkaur005 <amanpreetkaur00500@gmail.com>
2020-07-08 22:48:24 -0400

    CHANGES IN LOGIN INTERFACES

---
+++ app/src/main/java/com/example/clothingshop/LoginPage.java
@@ -0,0 +1,31 @@
+package com.example.clothingshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginPage extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        btn = (Button) findViewById(R.id.loginButton2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMenuActivity();
+            }
+        });
+    }
+    public void startMenuActivity(){
+        Intent intent= new Intent(this,ItemPage.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/clothingshop/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.clothingshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    Button LogIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.buttonToMain);
+        LogIn = (Button)findViewById(R.id.loginButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMenuActivity();
+            }
+        });
+
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNextActivity();
+            }
+        });
+    }
+    public void startMenuActivity(){
+        Intent intent= new Intent(this,ItemPage.class);
+        startActivity(intent);
+
+    }
+    public  void startNextActivity(){
+        Intent Next = new Intent(this, LoginPage.class);
+        startActivity(Next);
+
+    }
+}
+
+
