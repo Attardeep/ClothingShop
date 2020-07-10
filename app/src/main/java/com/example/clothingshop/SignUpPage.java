72027464b6ffeb442570f7be17871dd5f8f76224
ziwenemma <ziwenemma2017@gmail.com>
2020-07-09 20:26:36 -0400

    create a new sign up page

---
+++ app/src/main/java/com/example/clothingshop/SignUpPage.java
@@ -0,0 +1,18 @@
+package com.example.clothingshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+
+    public void LoginPage(View view) {
+    }
+}
