2896902c852100a8d65903aa017d4d37971e8497
ziwenemma <ziwenemma2017@gmail.com>
2020-07-09 20:37:51 -0400

    create a new sign up page

--- app/src/main/java/com/example/clothingshop/LoginPage.java
+++ app/src/main/java/com/example/clothingshop/LoginPage.java
@@ -9,22 +9,34 @@ import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
     Button btn;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
         btn = (Button) findViewById(R.id.loginButton2);
-
+        button = (Button) findViewById(R.id.Signup_button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startMenuActivity();
             }
         });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignupActivity();
+            }
+        });
     }
     public void startMenuActivity(){
         Intent intent= new Intent(this,ItemPage.class);
+        startActivity(intent);
+
+    }
+    public void startSignupActivity(){
+        Intent intent= new Intent(this,SignUpPage.class);
         startActivity(intent);
 
     }
--- app/src/main/java/com/example/clothingshop/SignUpPage.java
+++ app/src/main/java/com/example/clothingshop/SignUpPage.java
@@ -2,17 +2,46 @@ package com.example.clothingshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SignUpPage extends AppCompatActivity {
+    Button btn;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+        btn = (Button) findViewById(R.id.SignUpbutton);
+        button = (Button) findViewById(R.id.gobackButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNextActivity();
+            }
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMenuActivity();
+            }
+        });
+    }
+        public void startMenuActivity () {
+            Intent intent = new Intent(this, ItemPage.class);
+            startActivity(intent);
+
+        }
+        public void startNextActivity () {
+            Intent intent = new Intent(this, LoginPage.class);
+            startActivity(intent);
+
+        }
+
+        public void LoginPage (View view){
+        }
     }
 
-    public void LoginPage(View view) {
-    }
-}
