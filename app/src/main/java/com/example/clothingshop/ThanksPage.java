6753dea18aac23fdc83bb17f1c47314660d367ec
ziwenemma <ziwenemma2017@gmail.com>
2020-07-12 09:43:48 -0400

    thanks page added

--- app/src/main/java/com/example/clothingshop/ThanksPage.java
+++ app/src/main/java/com/example/clothingshop/ThanksPage.java
@@ -2,13 +2,32 @@ package com.example.clothingshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ThanksPage extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thanks_page);
+
+        btn = (Button) findViewById(R.id.new_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMenuActivity();
+            }
+        });
+    }
+    public void startMenuActivity(){
+        Intent intent= new Intent(this, ItemPage.class);
+        startActivity(intent);
+
     }
 }
+
+
