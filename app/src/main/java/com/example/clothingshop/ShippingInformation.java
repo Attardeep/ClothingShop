40ca71c1eb3e0d31df4dcd25634a25f2b03793b4
Neeraj <neerarjathi81@gmail.com>
2020-09-12 12:53:11 +0530

    Revert "Shipping Information saved on firebase"
    This reverts commit 9b3c5a4f

--- app/src/main/java/com/example/clothingshop/ShippingInformation.java
+++ app/src/main/java/com/example/clothingshop/ShippingInformation.java
@@ -9,7 +9,6 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -52,7 +51,6 @@ public class ShippingInformation extends AppCompatActivity implements AdapterVie
 
         sharedPreferences = getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE);
         final String sId = sharedPreferences.getString("id", "");
-        Log.e("Id", sId);
         if (!TextUtils.isEmpty(sId)) {
             DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("shippings");
 
