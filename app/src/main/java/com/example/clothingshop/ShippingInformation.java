2d3e135f4e81784d4e8562bc894f9a77307b40c8
Neeraj <neerarjathi81@gmail.com>
2020-09-12 12:53:39 +0530

    Revert "Shipping Information saved on firebase"
    This reverts commit 73b284aa

--- app/src/main/java/com/example/clothingshop/ShippingInformation.java
+++ app/src/main/java/com/example/clothingshop/ShippingInformation.java
@@ -1,129 +1,34 @@
 package com.example.clothingshop;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Objects;
-
-public class ShippingInformation extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ShippingInformation extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     Button btn;
 
-    String[] countries = {"Canada", "America", "Australia", "England", "China", "Mexico"};
-    EditText etName, etPhone, etAddress, etEmail;
-    String name, phone, address, email, country;
-    Spinner spino;
-    SharedPreferences sharedPreferences;
+    String[] countries = { "Canada","America","Australia","England","China","Mexico" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shipping_information);
 
-        spino = findViewById(R.id.countryChoose);
+        Spinner spino = findViewById(R.id.countryChoose);
         spino.setOnItemSelectedListener(this);
-
-        etName = findViewById(R.id.name);
-        etPhone = findViewById(R.id.phone);
-        etAddress = findViewById(R.id.address);
-        etEmail = findViewById(R.id.email);
-
-        sharedPreferences = getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE);
-        final String sId = sharedPreferences.getString("id", "");
-        if (!TextUtils.isEmpty(sId)) {
-            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("shippings");
-
-            databaseReference.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot snapshot) {
-
-
-                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-
-                        Shipping shipping = dataSnapshot.getValue(Shipping.class);
-
-                        if (TextUtils.equals(shipping.getShippingId(), sId)) {
-                            etName.setText(shipping.getName());
-                            etAddress.setText(shipping.getAddress());
-                            etEmail.setText(shipping.getEmail());
-                            etPhone.setText(shipping.getPhone());
-
-                        }
-
-
-                    }
-
-
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-
-                }
-            });
-
-        }
 
         btn = (Button) findViewById(R.id.confirm);
         btn.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
-
-                name = etName.getText().toString();
-                phone = etPhone.getText().toString();
-                address = etAddress.getText().toString();
-                email = etEmail.getText().toString();
-                country = spino.getSelectedItem().toString();
-
-                if (TextUtils.isEmpty(name)) {
-                    etName.setError("This field is empty!");
-                    return;
-                }
-                // blan space is for error messages
-                if (TextUtils.isEmpty(email)) {
-                    etEmail.setError("This field is empty!");
-                    return;
-                }
-                if (TextUtils.isEmpty(phone)) {
-                    etPhone.setError("the Phone number length should be 10");
-                    return;
-
-                }
-
-                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("shippings");
-                String Id = mDatabase.push().getKey();
-                Shipping user = new Shipping(Id, name, email, phone, address, country);
-                mDatabase.child(Objects.requireNonNull(Id)).setValue(user);
-
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("id", Id);
-                editor.apply();
-
-                Toast.makeText(ShippingInformation.this, "Shipping Information Saved Successfully!", Toast.LENGTH_LONG).show();
-
                 startMenuActivity();
             }
         });
@@ -139,14 +44,15 @@ public class ShippingInformation extends AppCompatActivity implements AdapterVie
                         .simple_spinner_dropdown_item);
         spino.setAdapter(ad);
 
-
     }
+        public void startMenuActivity(){
+            Intent intent= new Intent(this,PaymentPage.class);
+            startActivity(intent);
 
-    public void startMenuActivity() {
-        Intent intent = new Intent(this, PaymentPage.class);
-        startActivity(intent);
+        }
 
-    }
+
+
 
 
     @Override
